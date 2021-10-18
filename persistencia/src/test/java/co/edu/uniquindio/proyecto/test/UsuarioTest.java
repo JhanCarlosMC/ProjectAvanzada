0487a08d759855839db0d5a8e571f8e93c9eea16
JhanCarlosMC <jhanc.martinezc@uqvirtual.edu.co>
2021-10-18 16:26:10 -0500

    Test usuario agregado

--- persistencia/src/test/java/co/edu/uniquindio/proyecto/test/UsuarioTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/proyecto/test/UsuarioTest.java
@@ -1,9 +1,36 @@
 package co.edu.uniquindio.proyecto.test;
 
+import co.edu.uniquindio.proyecto.repositorios.UsuarioRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioTest {
+
+    @Autowired
+    private UsuarioRepo usuarioRepo;
+
+    //----------------------------------Metodos CRUD Test----------------------------------------
+    @Test
+    public void registrarUsuarioTest(){
+
+    }
+
+    @Test
+    public void eliminarUsuarioTest(){
+
+    }
+
+    @Test
+    public void actualizarUsuarioTest(){
+
+    }
+
+    @Test
+    public void listarUsuariosTest(){
+
+    }
 }
