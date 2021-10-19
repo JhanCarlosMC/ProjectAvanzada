1d9ea1e4b8f9efff07cfe29d1865991e1f4cbe3a
Michael Honores <michaels.honoresq@uqvirtual.edu.co>
2021-10-19 10:15:32 -0500

    Se agregan el test de Mensaje

--- persistencia/src/test/resources/dataSet.sql
+++ persistencia/src/test/resources/dataSet.sql
@@ -6,3 +6,6 @@
 
 
 insert into usuario values ("1", "Jcmc@gmail.com", "Jhan", "100232");
+insert into Mensaje values("1", "pedro", "2020-12-01", "hola", null);
+insert into Mensaje values("2", "pedro", "2020-12-01", "hola",null);
+insert into Mensaje values("3", "pedro", "2020-12-01", "hola", null);
